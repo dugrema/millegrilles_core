175740588a4197260c142ec270213b5494d5d408
Mathieu Dugre <mathieu.dugre@mdugre.info>
2024-10-21 14:20:11 -0400

    Fix consignation_id being removed on presence

--- src/topology_events.rs
+++ src/topology_events.rs
@@ -249,6 +249,7 @@ where M: ValidateurX509 + GenerateurMessages + MongoDao
 
     // Retirer champ cle
     set_ops.remove("instance_id");
+    set_ops.remove("consignation_id");  // Ce setting est manuel
     set_ops.insert("applications", applications);
     set_ops.insert("date_presence", estampille);
 
